1cdf53b1ab66273a18b1a5f25af2b0617c606c64
luka <lukagligorevic@gmail.com>
2022-01-29 16:43:28 +0100

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -5,9 +5,9 @@ var app = express();
 console.log("Hello World");
 
 // 2. Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path.
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
 
 // 3. Send the /views/index.html file as a response to GET requests to the / path. If you view your live app, you should see a big HTML heading (and a form that we will use later…), with no style applied.
 const absolutePath = __dirname;
@@ -16,3 +16,7 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
+
+// 4. Serve Static Assets
+const absolutePath = __dirname;
+app.use("/", express.static(absolutePath + "/public")); // express.static(path) => middlware -> path = absolute path of the folder containtg the assets
