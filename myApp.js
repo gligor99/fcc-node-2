a8f0906cd01e2fa82ac52a0d3daeea16b051986a
luka <lukagligorevic@gmail.com>
2022-01-29 16:33:39 +0100

    Serve a HTML File

--- myApp.js
+++ myApp.js
@@ -1,45 +1,17 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
+// 1. Modify the myApp.js file to log "Hello World" to the console.
 console.log("Hello World");
 
-app.get('/', (req, res) => {
-    res.send("Hello Express")
-})
+// 2. Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path.
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
+// 3. Send the /views/index.html file as a response to GET requests to the / path. If you view your live app, you should see a big HTML heading (and a form that we will use later…), with no style applied.
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath + "/views/index.html");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
